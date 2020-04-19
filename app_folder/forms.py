607fbbf4d605910e3fbecee201967a2fa6791ec9
Minli He <minli.he@sjsu.edu>
2020-04-19 09:47:11 -0700

    Update forms.py
    import validators

--- app_folder/forms.py
+++ app_folder/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError, validators
 from wtforms.validators import DataRequired, Email, DataRequired, EqualTo
 from .models import User
 
