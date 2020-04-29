118c9f5a63af94fabaee2e9abe13ec2ffce1cb8a
“IuDeDaShu” <thllmxx@gmail.com>
2020-04-29 11:17:55 -0700

    email add

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -2,22 +2,28 @@ from flask import render_template
 from flask import redirect
 from flask import flash, url_for
 from app_folder import app, db, login_manager
-from .forms import LoginForm, RegistrationForm, DeleteAccountForm, AvailabitityForm, MeetingsForm, ScheduledMeetingForm
-from .models import User
+from .forms import LoginForm, RegistrationForm,DeleteAccountForm,AvailabitityForm,MeetingsForm,EmailConfirmationForm
+from .models import User, Availability,Meetings
 import flask_login
 import flask
 from flask_login import login_user,login_required, logout_user
 from wtforms import ValidationError
 from flask import request
-import datetime
-from .models import CustomHTMLCalendar
-from flask_wtf import FlaskForm
-from datetime import date
-from wtforms.fields.html5 import DateField
-from wtforms.fields.html5 import DateTimeField
+from flask_mail import Mail, Message
 
 current_user = flask_login.current_user
-cal = CustomHTMLCalendar(0)
+
+
+mail= Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = '2020131springteam6@gmail.com'
+app.config['MAIL_PASSWORD'] = 'abc!123ab'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -25,10 +31,10 @@ def load_user(user_id):
     This method takes the ID of a user and returns the corresponding user object.
 
     Args:
-         user_id (String) : The ID that will be used to find a corresponding user in the data base.
+         user_id (String) : The ID that will be used to find a corresponding user in the data base. 
 
-    Returns:
-            The user Object that corresponds with the user_id that was passed or none if there is no corresponding user.
+    Returns: 
+            The user Object that corresponds with the user_id that was passed or none if there is no corresponding user. 
     '''
     return User.query.get(int(user_id))
 
@@ -39,17 +45,17 @@ def index():
     '''This method creates the webpage that will display when a guest first visits the application.
 
     Returns:
-            The HTML template that will be rendered when a guest first visits the application. This is the home page.
+            The HTML template that will be rendered when a guest first visits the application. This is the home page. 
     '''
-    return render_template('index.html', title='Home', User=User, current_user=current_user,cal=cal.formatmonth(datetime.date.today().year, datetime.date.today().month))
+    return render_template('index.html', title='Home', User=User, current_user=current_user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     '''
-    This method creates the webpage that will display when a guest goes to log into their account.
+    This method creates the webpage that will display when a guest goes to log into their account. 
 
     Returns:
-            the HTML template that will be rendered when a guest wants to log into their account.
+            the HTML template that will be rendered when a guest wants to log into their account. 
     '''
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -67,10 +73,10 @@ def login():
 @app.route('/logout')
 def logout():
     '''
-    This method creates the webpage that will display when a guest logs out of the application.
+    This method creates the webpage that will display when a guest logs out of the application. 
 
     Returns:
-            The HTML template that will be rendered when a guest wants to log out of the application.
+            The HTML template that will be rendered when a guest wants to log out of the application.  
     '''
     logout_user()
     return redirect(url_for('index'))
@@ -79,10 +85,10 @@ def logout():
 @app.route('/createAccount', methods=['GET', 'POST'])
 def createAccount():
     '''
-    This method creates the webpage that will display when a guest wants to create an account.
+    This method creates the webpage that will display when a guest wants to create an account. 
 
-    Returns:
-            The HTML template that will be rendered when a guest wants to create an account.
+    Returns: 
+            The HTML template that will be rendered when a guest wants to create an account. 
     '''
     if current_user.is_authenticated:
         return redirect (url_for('index'))
@@ -106,77 +112,80 @@ def createAccount():
             flash(e)
     return render_template('createAccount.html', title='Create Account', form=current_form)
 
-'''
-@app.route('/createEvent', methods=['GET', 'POST'])
-def createEvent():
-    if request.method == 'POST':
-            meetingDate = request.form['meetingDate']
-            meetingTime = request.form['meetingTime']
-            descriptionOfMeeting = request.form['descriptionOfMeeting']
-            participants = request.form['participants']
+import time
 
-    db.session.add(meetingDate)
-    db.session.commit()
-
-    db.session.add(meetingTime)
-    db.session.commit()
-
-    db.session.add(descriptionOfMeeting)
-    db.session.commit()
-
-    db.session.add(participants)
-    db.session.commit()
-    
-    return render_template('creatEvent.html', title='Schedule A Meeting', meetingDate = meetingDate, 
-                            meetingTime = meetingTime, descriptionOfMeeting = descriptionOfMeeting,
-                            participants = participants)
-'''
-@app.route('/meetingsPage', methods=['GET', 'POST'])
-def meetingsPage():
-    form = ScheduledMeetingForm()
-    return render_template('meetingsPage.html', title='Scheduled Meeting List', form = form)
-
-
+def isTimeFormat(input):
+    try:
+        time.strptime(input, '%H:%M')
+        return True
+    except ValueError:
+        return False
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end		
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
 	delete_acc_form = DeleteAccountForm(request.form)
 	availability_form = AvailabitityForm(request.form)
+	email_confirmation_form = EmailConfirmationForm(request.form)
 	meetings_form = MeetingsForm(request.form)
 	current_user = flask_login.current_user
 	if request.method == 'POST':
-		if 'Delete Account' == request.form['submit'] :
+		if 'Delete Account' == request.form['submit'] :			
 			db.session.delete(current_user)
 			db.session.commit()
 			print("Deleting account")
 			return redirect(url_for('login'))
 
-		elif 'Set Availability' == request.form['submit']:
+		elif 'Set Availability' == request.form['submit']:			
 			availability = Availability(user_id=current_user.id,from_time=availability_form.from_time.data,to_time=availability_form.to_time.data)
-			if current_user.availability:
-				availability = Availability.query.get(current_user.availability.id)
-				availability.from_time = availability_form.from_time.data
-				availability.to_time = availability_form.to_time.data
-				db.session.merge(availability)
-				db.session.commit()
-				print(availability.user)
+			if not isTimeFormat(availability_form.from_time.data) or not  isTimeFormat(availability_form.to_time.data) or availability_form.from_time.data > availability_form.to_time.data:
+				flash('**Invalid availability!**')			
 			else:
-				db.session.add(availability)
-				db.session.commit()
-			print("Setting availability")
-			current_user = User.query.get(int(current_user.id))
-
+				if not time_in_range("09:00","22:00",availability_form.from_time.data) or not time_in_range("09:00","22:00",availability_form.to_time.data):
+					flash('**Invalid range choose between 09:00 am  and 10:00 pm!**')
+				else:
+					if current_user.availability: 
+						availability = Availability.query.get(current_user.availability.id)
+						availability.from_time = availability_form.from_time.data
+						availability.to_time = availability_form.to_time.data
+						db.session.merge(availability)
+						db.session.commit()
+						print(availability.user)				
+					else:
+						db.session.add(availability)
+						db.session.commit()
+					print("Setting availability")
+					current_user = User.query.get(int(current_user.id))
+		
 		elif 'Set Meetings length' == request.form['submit']:
 			meetings = Meetings(user_id=current_user.id,length=meetings_form.length.data)
-			if current_user.meetings:
-				meetings = Meetings.query.get(current_user.meetings.id)
-				meetings.length = meetings_form.length.data
-				db.session.merge(meetings)
-				db.session.commit()
-				print(meetings.user)
-			else:
-				db.session.add(meetings)
-				db.session.commit()
-			print("Setting meetings length")
+			if  str(meetings_form.length.data) != "00:15" and str(meetings_form.length.data) != "00:30" and str(meetings_form.length.data) != "01:00" :
+				flash("use 00:15, 00:30 minutes or 01:00 hour  slots"+str(meetings_form.length.data)+ str(str(meetings_form.length.data) == "00:15"))
+			else:			
+				if current_user.meetings:
+					meetings = Meetings.query.get(current_user.meetings.id)
+					meetings.length = meetings_form.length.data
+					db.session.merge(meetings)
+					db.session.commit()
+					print(meetings.user)				
+				else:
+					db.session.add(meetings)
+					db.session.commit()
+				print("Setting meetings length")
+				current_user = User.query.get(int(current_user.id))
+		elif 'Set Email Confirmation' == request.form['submit']:			
+			current_user.email_confirmation = email_confirmation_form.confirmation.data
+			db.session.merge(current_user)
+			db.session.commit()
 			current_user = User.query.get(int(current_user.id))
-
-	return render_template('settings.html', title='Settings', form=delete_acc_form,availability_form=availability_form,user=current_user,meetings_form=meetings_form)
+			
+			msg = Message('Hello', sender = '2020131springteam6@gmail.com', recipients = ['thllmxx@gmail.com'])
+			msg.body = "Hello Flask message sent from Flask-Mail"
+			mail.send(msg)
+			
+		
+	return render_template('settings.html', title='Settings', form=delete_acc_form,availability_form=availability_form,user=current_user,meetings_form=meetings_form,email_form=email_confirmation_form)
