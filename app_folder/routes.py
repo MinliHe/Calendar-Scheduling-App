5f343f8fec637148fd746774bb5b9b0ce7b6b9f6
Shana Nguyen <shananguyen01@gmail.com>
2020-05-02 17:42:37 -0700

    updated meetingsPage to be default home page when logged in

--- app_folder/routes.py
+++ app_folder/routes.py
@@ -68,7 +68,7 @@ def login():
             flash('**Invalid username or password**')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect('index')
+        return redirect('meetingsPage')
     return render_template('login.html', title='Sign In', form=form)
 
 
